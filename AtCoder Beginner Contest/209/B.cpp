8ca7151022a2b1094b08a43dbbd7698e87a1b8c5
Miracle- <wmzhuo1996@163.com>
2021-07-10 23:13:05 +0800

    Create B.cpp

---
+++ AtCoder Beginner Contest/209/B.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define hash _hash
+#define endl '\n'
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define cal1(x) __builtin_popcount(x)
+using namespace std;
+using namespace atcoder;
+using ll = int64_t;
+using int128 = __int128_t;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+
+const ld pi = M_PI;
+const int N = 2e5 + 10;
+string str; int n, m, a[N];
+
+template <class T> inline T &chmin(T &x, const T &y){ return x = min(x, y); }
+template <class T> inline T &chmax(T &x, const T &y){ return x = max(x, y); }
+
+string solve(void)
+{
+	int res = 0;
+	for (int k = 1; k <= n; ++k) {
+		res += (k & 1) ? a[k] : a[k] - 1;
+	}
+	return (res <= m) ? "Yes" : "No";
+}
+
+string solution(void)
+{
+	int res = 0;
+	res = accumulate(a + 1, a + n + 1, 0) - n / 2;
+	return (res <= m) ? "Yes" : "No";
+}
+
+signed main(int argc, char** argv)
+{
+	#ifdef Miracle
+		auto start_clock = clock();
+		freopen("input.txt", "r", stdin);
+	//	freopen("output.txt", "w", stdout);
+	#endif
+	std::ios::sync_with_stdio(false); cin.tie(nullptr);
+	while (cin >> n >> m) {
+		for (int k = 1; k <= n; ++k) cin >> a[k];
+		cout << solution() << endl;
+	}
+	#ifdef Miracle
+        auto end_clock = clock();
+        cout << end_clock - start_clock << "ms" << endl;
+    #endif
+	return 0;	
+}
+// type: implementation
+
+/* tips */
+/*
+	number of even: n / 2
+	sum - n / 2
+*/
+
+/* editorial */
+/*
+*/
+
+/* question */
+/*
+*/
