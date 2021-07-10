899183be6e7f88efc1de6e11171f4082db1a02d1
Miracle- <wmzhuo1996@163.com>
2021-07-10 23:52:43 +0800

    Create C.cpp

---
+++ AtCoder Beginner Contest/172/C.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define hash _hash
+#define endl '\n'
+#define sz(x) ((int)x.size())
+#define all(x) (x).begin(), (x).end()
+#define cal1(x) __builtin_popcount(x)
+using namespace std;
+using namespace atcoder;
+using ll = int64_t;
+using int128 = __int128_t;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pli = pair<ll, int>;
+
+const ld pi = M_PI;
+const int N = 2e5 + 10;
+string str; int n, m, q; ll a[N], b[N];
+
+template <class T> inline T &chmin(T &x, const T &y){ return x = min(x, y); }
+template <class T> inline T &chmax(T &x, const T &y){ return x = max(x, y); }
+
+int solve(void)
+{
+	vector<ll> sa(n + 1, 0);
+	vector<ll> sb(m + 1, 0);
+	partial_sum(a + 1, a + n + 1, sa.begin() + 1);
+	partial_sum(b + 1, b + m + 1, sb.begin() + 1);
+	int res = 0;
+	for (int l = 0, r = m; l <= n; ++l) {
+		while (r && sa[l] + sb[r] > q) --r;
+		if (sa[l] + sb[r] <= q) chmax(res, r + l);
+	}
+	return res;
+}
+
+signed main(int argc, char** argv)
+{
+	#ifdef Miracle
+		auto start_clock = clock();
+		freopen("input.txt", "r", stdin);
+	//	freopen("output.txt", "w", stdout);
+	#endif
+	std::ios::sync_with_stdio(false); cin.tie(nullptr);
+	while (cin >> n >> m >> q) {
+		for (int k = 1; k <= n; ++k) cin >> a[k];
+		for (int k = 1; k <= m; ++k) cin >> b[k];
+		cout << solve() << endl;
+	}
+	#ifdef Miracle
+        auto end_clock = clock();
+        cout << end_clock - start_clock << "ms" << endl;
+    #endif
+	return 0;	
+}
+// type: partial-sum + two-pointers
+
+/* tips */
+/*
+	sa[l] + sb[r] <= q
+*/
+
+/* editorial */
+/*
+*/
+
+/* question */
+/*
+*/
